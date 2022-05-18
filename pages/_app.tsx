c09e4fd5abdd134e256d4d685b06bc6728aab838
Mariano Manzo <marianomanzo@MacBook-Pro-de-Mariano.local>
2022-05-18 13:34:27 -0300

    beta6

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -99,6 +99,12 @@ const App: React.FC<AppProps> = ({Component, pageProps}) => {
                 </Stack>
               </Stack>
             </Stack>
+            <Link href='#' isExternal>
+            KIDS 
+</Link>
+      <Link href='https://www.mofletes.com.ar/' >
+         BABYS 
+</Link>
             <Component {...pageProps} />
           </Stack>
           <Skeleton endColor="blue.300" height="40px" startColor="violet">
